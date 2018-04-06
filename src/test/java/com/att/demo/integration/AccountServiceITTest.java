994339c649ba2c4809423866581b2578b9275b07
bijay <bijay@10.20.11.202>
2018-04-06 15:54:11 -0500

    Sabita Subedi- Test Passed.

--- src/test/java/com/att/demo/integration/AccountServiceITTest.java
+++ src/test/java/com/att/demo/integration/AccountServiceITTest.java
@@ -5,15 +5,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class AccountServiceITTest {
+	
+	//Test
 
 	@Test
 	public void testSetUp() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testTestfindAllAccount_success() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 }
